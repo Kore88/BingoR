a9f4e94ee79edc4656e73cf3a2126aa2a6ac6d6e
Kőrösi Zoltán <prokore88@gmail.com>
2024-03-23 09:54:59 +0100

    Add project files.

---
+++ BingoR/BingoJatekos.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BingoR
+{
+    internal class BingoJatekos
+    {
+        string nev;
+
+        public string Nev { get => nev; set => nev = value; }
+
+        public string[,] Szamok;
+        public bool[,] Kihuzva;
+
+        public BingoJatekos(string forras)
+        {
+            Szamok = new string[5, 5];
+            Kihuzva = new bool[5, 5];
+            Kihuzva[2, 2] = true;
+            Nev = forras.Split('.')[0];
+
+            try
+            {
+                using FileStream fs = File.OpenRead(forras);
+                using StreamReader sr = new StreamReader(fs);
+                int sorIndex = 0;
+                    while (!sr.EndOfStream)
+                {
+                    string[] st = sr.ReadLine().Split(';');
+                    for (int oszlopindex = 0; oszlopindex < st.Length; oszlopindex++)
+                    {
+                        Szamok[sorIndex,oszlopindex] = st[oszlopindex];
+                    }
+                    sorIndex++;
+
+                }
+                    fs.Close();
+                    sr.Close();
+
+
+            }
+            catch (Exception ex) { MessageBox.Show("A fájl nem található!"); }
+        }
+
+        public void SoroltSzamotJelol(int kiszam)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (Szamok[i,j] == kiszam.ToString())
+                    {
+                        Kihuzva[i,j] = true;
+                    }
+                }
+            }
+        }
+        public bool BingoEll()
+        {
+            for ( int sor = 0;sor < 5;sor++)
+            {
+                if (Kihuzva[sor,0] && Kihuzva[sor,1] &&
+                    Kihuzva[sor, 2] && Kihuzva[sor, 3] &&
+                    Kihuzva[sor, 4]
+                    
+                    ) return true;
+            }
+            for (int oszlop = 0; oszlop < 5; oszlop++)
+            {
+                 if(Kihuzva[oszlop, 0] && Kihuzva[oszlop, 1] &&
+                    Kihuzva[oszlop, 2] && Kihuzva[oszlop, 3] &&
+                    Kihuzva[oszlop, 4]
+                    ) return true;
+            }
+            if (Kihuzva[0,0] && Kihuzva[1,1] && Kihuzva[3,3] && Kihuzva[4,4]
+                ) return true;
+               if( Kihuzva[0, 4] && Kihuzva[1, 3] && Kihuzva[3, 2] && Kihuzva[4, 0]
+                )return true;
+            return false;
+        }
+    }
+}
---
+++ BingoR/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BingoR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+        string[,] jatekosSzamok;
+        List<int> kihuzottszamok = new List<int>();
+        List<int> nyertesek = new List<int>();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Beolvas();
+            
+        }
+
+        private void Beolvas()
+        {
+            try
+            {
+                using FileStream fs = File.OpenRead("nevek.text");
+                using StreamReader sr = new StreamReader(fs);
+
+                int index = 0;
+
+                while (!sr.EndOfStream)
+                {
+                    jatekosok.Add(new BingoJatekos(sr.ReadLine()));
+                    cbxJatekos.Items.Add(jatekosok[index].Nev);
+                    nyertesek.Add(0);
+                    index++;
+                }
+                fs.Close();
+                sr.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void cbxJatekos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txbNyeroSzamok.Text = "";
+
+            jatekosSzamok = jatekosok[cbxJatekos.SelectedIndex].Szamok;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for(int j = 0; j < 5; j++)
+            //    {
+            //        TextBox tb = new TextBox();
+            //        tb.Margin = new Thickness(j*30, i*30,0,0);
+            //        tb.Width = 20;
+                    
+            //        if (i == 2&& j == 2)
+            //        {
+            //            tb.Text = "X";
+            //            tb.Background = Brushes.Green;
+            //        }
+            //        else
+            //        {
+            //            tb.Text = jatekosSzamok[i, j];
+            //        }
+            //        cTarto.Children.Add(tb);
+            //    }
+            //}
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    TextBox tb = new TextBox();
+                    tb.Margin = new Thickness(j * 30, i * 30, 0, 0);
+                    tb.Width = 20;
+
+                    if (i == 2 && j == 2)
+                    {
+                        tb.Text = "X";
+                        tb.Background = Brushes.Green;
+                    }
+                    else if (jatekosok[cbxJatekos.SelectedIndex].Kihuzva[i, j])
+                    {
+                        tb.Text = jatekosSzamok[i, j];
+                        tb.Background = Brushes.Green;
+
+                    }
+                    else
+                    {
+                        tb.Text = jatekosSzamok[i, j];
+                    }
+                    cTarto.Children.Add(tb);
+                }
+            }
+        }
+
+        private void btnHuzas_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbxJatekos.SelectedIndex != 0)
+            {
+                Random rnd = new Random();
+                int vszam;
+
+
+                do
+                {
+                    vszam = rnd.Next(1, 76);
+                } while (kihuzottszamok.Contains(vszam));
+
+                kihuzottszamok.Add(vszam);
+                txbNyeroSzamok.Text += (vszam.ToString() + ", ");
+
+                if (kihuzottszamok.Count %10 ==0)
+                {              
+                    txbNyeroSzamok.AppendText("\n");
+                    txbNyeroSzamok.Height += 23;
+                }
+
+                jatekosok[cbxJatekos.SelectedIndex].SoroltSzamotJelol(vszam);
+
+                cTarto.Children.Clear();
+
+                bool nyert = false;
+
+                foreach (BingoJatekos jatekos in jatekosok)
+                {
+                    for (int i = 0; i < jatekosok.Count; i++)
+                    {
+                        if (nyertesek[i] == 0) 
+                        {
+                            jatekos.SoroltSzamotJelol(vszam);
+                            if (jatekos.BingoEll())
+                            {
+                                nyertesek[i] = 1;
+                                if (!nyert)
+                                {
+                                    nyert = true;
+                                    MessageBox.Show($"{jatekos.Nev} Gratulálok, BINGÓ !!");
+                                }
+                                break;
+
+                            }
+                        }         
+                    }                    
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        TextBox tb = new TextBox();
+                        tb.Margin = new Thickness(j * 30, i * 30, 0, 0);
+                        tb.Width = 20;
+
+                        if (i == 2 && j == 2)
+                        {
+                            tb.Text = "X";
+                            tb.Background = Brushes.Green;
+                        }
+                        else if (jatekosok[cbxJatekos.SelectedIndex].Kihuzva[i, j])
+                        {
+                            tb.Text = jatekosSzamok[i, j];
+                            tb.Background = Brushes.Green;
+
+                        }
+                        else
+                        {
+                            tb.Text = jatekosSzamok[i, j];
+                        }
+                        cTarto.Children.Add(tb);
+                    }
+                }
+
+                //if (jatekosok[cbxJatekos.SelectedIndex].BingoEll())
+                //{
+                //    MessageBox.Show("BINGO !!!");
+                //}
+            }
+           
+        }      
+    }
+}
